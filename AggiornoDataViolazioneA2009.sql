b3676aaa0d2818cfa9ebe21a51fc91d52e63dfb2
MelissaCanu <148777539+MelissaCanu@users.noreply.github.com>
2024-02-09 14:46:26 +0100

    Aggiorno tabella per fare query giuste

---
+++ AggiornoDataViolazioneA2009.sql
@@ -0,0 +1,43 @@
+UPDATE VERBALE
+SET DataViolazione = '2009-02-10'
+WHERE DataTrascrizioneVerbale = '2009-02-10';
+
+UPDATE VERBALE
+SET DataViolazione = '2009-02-12'
+WHERE DataTrascrizioneVerbale = '2009-02-12';
+
+UPDATE VERBALE
+SET DataViolazione = '2009-02-14'
+WHERE DataTrascrizioneVerbale = '2009-02-14';
+
+UPDATE VERBALE
+SET DataViolazione = '2009-05-15'
+WHERE DataTrascrizioneVerbale = '2009-05-15';
+
+UPDATE VERBALE
+SET DataViolazione = '2009-04-16'
+WHERE DataTrascrizioneVerbale = '2009-04-16';
+
+UPDATE VERBALE
+SET DataViolazione = '2009-07-17'
+WHERE DataTrascrizioneVerbale = '2009-07-17';
+
+UPDATE VERBALE
+SET DataViolazione = '2010-02-17'
+WHERE DataTrascrizioneVerbale = '2010-02-17';
+
+UPDATE VERBALE
+SET DataViolazione = '2010-02-17'
+WHERE DataTrascrizioneVerbale = '2010-02-17';
+
+UPDATE VERBALE
+SET DataViolazione = '2010-02-17'
+WHERE DataTrascrizioneVerbale = '2010-02-17';
+
+UPDATE VERBALE
+SET DataViolazione = '2010-02-17'
+WHERE DataTrascrizioneVerbale = '2010-02-17';
+
+UPDATE VERBALE
+SET DataViolazione = '2010-02-17'
+WHERE DataTrascrizioneVerbale = '2010-02-17';
---
+++ AggiornoTabellaVerbale450Euro.sql
@@ -0,0 +1,2 @@
+INSERT INTO VERBALE (DataViolazione, IndirizzoViolazione, Nominativo_Agente, DataTrascrizioneVerbale, Importo, DecurtamentoPunti, IDanagrafica, IDviolazione)
+VALUES ('2024-02-09', 'Via Nuova, 123', 'Agente_Pippo', '2024-02-10', 450.00, 6, 1, 4);
